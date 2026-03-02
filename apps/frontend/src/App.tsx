81e265491efc034d6b58e9fa3ba2c3ae3e5f14b5
Jugal Mahida <jugal.mahida.work@gmail.com>
2026-03-02 22:16:40 +0530

    Project setup frontend, frontend_admin, docker compose files

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Docker MERN project</p>
+    </div>
+  );
+}
+
+export default App;
