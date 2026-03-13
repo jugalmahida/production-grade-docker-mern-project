06aeae8cf2b6f7cc9efdfb1ebd8fccdad6538401
Jugal Mahida <jugal.mahida.work@gmail.com>
2026-03-13 22:38:29 +0530

    Learned & Implement the new docker commands, add magic button in app.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -9,11 +9,16 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>
+    <div className="min-h-screen flex flex-col gap-5 items-center justify-center bg-gray-100">
+      <div>
         Docker MERN project | Backend Status :- {loading ? "Loading ..." : " "}
         {result?.status}
-      </p>
+      </div>
+      <div>
+        <a href="https://jugalmahida.com" target="_blank" className="bg-white px-3 py-2 rounded-2xl">
+          Don't Click
+        </a>
+      </div>
       <div className="mt-12">
         <p> </p>
         {error && (
